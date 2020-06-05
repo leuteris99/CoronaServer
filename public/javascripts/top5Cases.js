476ce196a1ccdd70bd968bf7d2f6123c31d3124d
p17anto2 <56692408+p17anto2@users.noreply.github.com>
2020-06-05 14:36:36 +0300

    Add files via upload

--- public/javascripts/top5Cases.js
+++ public/javascripts/top5Cases.js
@@ -1,6 +1,6 @@
-function ch1(country,startDate,endDate) {
+function ch1(startDate,endDate) {
     let request = new XMLHttpRequest();
-    request.open('GET', '/db/top-5-cases/' '&' + startDate + '&' + endDate);
+    request.open('GET', '/db/top-5-cases/' + startDate + '&' + endDate);
     request.responseType = 'text';
     request.onload = function () {
         console.log(request.response);
@@ -13,11 +13,11 @@ function createChart(data) {
     let titleEl = document.getElementById('title');
     titleEl.textContent = "Top 5 Countries By Cases";
     // preparing the data for the chart
-	
-    let dateRep = [];
+
+    let country = [];
     let i =0;
     data.forEach(element => {
-        dateRep[i++] = element['dateRep'];
+        country[i++] = element['country'];
     });
     let cases = [];
     i =0;
@@ -28,17 +28,25 @@ function createChart(data) {
     // rendering the chart
     var ctx = document.getElementById('chart').getContext('2d');
     var mychart = new Chart(ctx, {
-        type: 'line',
+        type: 'polarArea',
         data: {
-            labels: dateRep,
+            labels: country,
             datasets: [{
-                label: '# of cases in ' + country,
+                label: '# Top 5 ',
                 data: cases,
-                backgoundColor: [
-                    'rgba(255, 99, 132, 0.2)'
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(99,161,255,0.2)',
+                    'rgba(133,255,99,0.2)',
+                    'rgba(255,161,99,0.2)',
+                    'rgba(99,255,232,0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgb(99,120,255)',
+                    'rgb(109,255,99)',
+                    'rgb(255,167,99)',
+                    'rgb(99,255,224)'
                 ],
                 borderWidth: 1,
             }]
